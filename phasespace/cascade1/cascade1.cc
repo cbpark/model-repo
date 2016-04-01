ea94ccd6b5fa6dc4cb77d9a9d64b57af4672f95b
cbpark <cbpark@gmail.com>
2016-04-01 23:05:45 +0900

    Starting to write cascade1

--- phasespace/cascade1/cascade1.cc
+++ phasespace/cascade1/cascade1.cc
@@ -1,5 +1,19 @@
+/*
+ *  p p --> b1 + b2 --> h s + b2
+ */
+#include "TLorentzVector.h"
+#include <cmath>
 #include <iostream>
 
+TLorentzVector initProtons(double beamE) {
+    double protonM = 0.938272046;
+    TLorentzVector beam(0.0, 0.0, std::sqrt(beamE * beamE - protonM * protonM),
+                        beamE);
+    TLorentzVector target(0.0, 0.0, 0.0, protonM);
+    return beam + target;
+}
+
 int main() {
-    std::cout << "Hello, world!\n";
+    TLorentzVector init = initProtons(400.0);
+    init.Print();
 }
